e9a82d0716da50623a6195b6ba00ccd30d542e99
Fritz Metellus <jtec255@gmail.com>
2020-07-18 20:17:06 -0400

    react workshop week 4 modal form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,59 +1,148 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from "react";
+import { 
+	Card, 
+	CardImg,
+	CardText,
+	CardBody,
+	Breadcrumb,
+	BreadcrumbItem,
+	Button,
+	Label,
+	Modal,
+	ModalBody,
+	ModalHeader
+} from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
+const minLength = len => val => val && (val.length >= len);
+const maxLength = len => val => !val || (val.length <= len); 
+
+class CommentForm extends Component {
+	constructor(props){
+		super(props);
+		this.state = { visibility: false };
+
+		this.handleSubmit= this.handleSubmit.bind(this);
+	};
+
+
+	toggle = () => this.setState({ visibility: !this.state.visibility });
+
+	handleSubmit(values) {
+		this.toggle()
+        console.log('Current State is' + JSON.stringify(values));
+        alert('Current State is' + JSON.stringify(values));
+	}
+	
+	render() {
+		return (
+			<div>
+				<Modal isOpen={this.state.visibility} toggle={() => this.toggle()}>
+					<ModalHeader toggle={() => this.toggle()}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={this.handleSubmit}>
+							<div className = "form-group">
+								<Label htmlFor="rating">Rating</Label>
+								<Control.select model=".rating" id="rating" name="rating" className="form-control">
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+								</Control.select>
+							</div>
+							<div className = "form-group">		
+								<Label  htmlFor="author">Author</Label>
+								<Control.text model=".author" id="author" name="author" className="form-control"
+									validators = {{
+										minLength: minLength(2),
+										maxLength: maxLength(15)
+									}}
+								/>
+								<Errors 
+									className="text-danger"
+									model=".author"
+									show="touched"
+									component="div"
+									messages = {{
+                                        minLength: 'Must be at least 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+									}}
+								/>
+							</div>
+							<div className = "form-group">		
+								<Label  htmlFor="text">Comment</Label>
+								<Control.textarea rows="6" model=".text" id="text" name="text" className="form-control"/>
+							</div>
+							<Button type = "submit" color="primary" id="HandleSubmit">Submit</Button>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+				<Button outline onClick={() => this.toggle()}>
+					<i className="fa fa-pencil" aria-hidden />
+					Submit Comment 	
+				</Button>
+			</div>
+		);
+	}
+}
+
 function RenderCampsite({campsite}) {
-    return (
-        <div className="col-md-5 m-1">
-            <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>
-    );
+	return (
+		<div className="col-md-5 m-1">
+			<Card>
+				<CardImg top src={campsite.image} alt={campsite.name} />
+				<CardBody>
+					<CardText>{campsite.description}</CardText>
+				</CardBody>
+			</Card>
+		</div>
+	);
 }
 
 function CampsiteInfo(props) {
-    if (props.campsite) {
-        return (
-            <div className="container">
-                 <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div> 
-        );    
-    }
-    return <div />    
+	if (props.campsite) {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<Breadcrumb>
+							<BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+							<BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+						</Breadcrumb>
+						<h2>{props.campsite.name}</h2>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
+					<RenderCampsite campsite={props.campsite} />
+					<RenderComments comments={props.comments} />
+				</div>
+			</div> 
+		);    
+}
+return <div />    
 }
 
 function RenderComments({comments}) {
-    if(comments) {
-        return(<div className="col-md-5 m-1">
-                <h4>Comments</h4>
-                {comments.map((comment)=> {
-                    return (
-                        <div key={comment.id}>
-                            <p>{comment.text}</p>
-                            <p> -- {comment.author}  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                        </div>
-                    )
-                })}
-                </div>)
-    }
-    return <div />
+	if(comments) {
+		return(
+			<div className="col-md-5 m-1">
+				<h4>Comments</h4>
+				{comments.map((comment)=> {
+					return (
+						<div key={comment.id}>
+							<p>{comment.text}</p>
+							<p> -- {comment.author}  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+						</div>
+					)
+				})}
+				<CommentForm />
+			</div>
+		)
+	}
+	return <div />
 }
 
 
