ef3e074c8af9674b63c1d17f121338803c1cb06e
Fritz Metellus <jtec255@gmail.com>
2020-07-22 15:33:03 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -31,8 +31,7 @@ class CommentForm extends Component {
 
 	handleSubmit(values) {
 		this.toggle()
-        console.log('Current State is' + JSON.stringify(values));
-        alert('Current State is' + JSON.stringify(values));
+		this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
 	}
 	
 	render() {
@@ -117,7 +116,11 @@ function CampsiteInfo(props) {
 				</div>
 				<div className="row">
 					<RenderCampsite campsite={props.campsite} />
-					<RenderComments comments={props.comments} />
+					<RenderComments 
+						comments={props.comments}
+						addComment={props.addComment}
+                        campsiteId={props.campsite.id} 
+					/>
 				</div>
 			</div> 
 		);    
@@ -125,7 +128,7 @@ function CampsiteInfo(props) {
 return <div />    
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}){
 	if(comments) {
 		return(
 			<div className="col-md-5 m-1">
@@ -138,7 +141,7 @@ function RenderComments({comments}) {
 						</div>
 					)
 				})}
-				<CommentForm />
+				<CommentForm campsiteId={campsiteId} addComment={addComment} />
 			</div>
 		)
 	}
